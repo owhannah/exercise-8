513864f88bbe9545dcb677ca52bf7af995728a2d
Your Name <you@example.com>
2024-01-24 16:55:08 +0800

    print_the_following_pattern

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -6,3 +6,11 @@
 # 3 3 3 
 # 4 4 4 4 
 # 5 5 5 5 5
+
+def print_number_pattern(rows):
+    for pattern in range(1, rows + 1):
+        print((str(pattern) + ' ') * pattern)
+
+# Given example:
+number_of_rows = 5
+print_number_pattern(number_of_rows)
